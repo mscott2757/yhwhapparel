53df5bc66421e9da979f8db03fb1919d54c9140c
Dominic <dfannjiang@gmail.com>
2017-03-06 22:33:23 -0800

    made default value for affirms 0

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,3 +1,8 @@
 class Prayer < ActiveRecord::Base
   validates :prayer, presence: true
+  after_initialize :set_defaults, unless: :persisted?
+
+  def set_defaults
+    self.affirms ||= 0
+  end
 end
