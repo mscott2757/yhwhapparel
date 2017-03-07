ac21eac57ae4c285cace8b3cd67ded1042d7e741
Dominic <dfannjiang@gmail.com>
2017-03-06 22:31:27 -0800

    added affirms attribute to prayer model (again cause i deleted it)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307062009) do
+ActiveRecord::Schema.define(version: 20170307063100) do
 
   create_table "nifty_key_value_store", force: true do |t|
     t.integer "parent_id"
