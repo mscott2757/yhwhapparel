759f58fd5aefd9af1e50e4a3d3a1f74137a4402d
Mason Scott <mscott2757@berkeley.edu>
2017-04-02 10:46:42 -0700

    removed shoppe from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'prayers/new'
-
-  mount Shoppe::Engine => "/shoppe"
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
