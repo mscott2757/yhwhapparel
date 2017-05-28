82097d18d6c8cb34db503d3841afe5b94f0c21a9
Mason Scott <mscott2757@berkeley.edu>
2017-05-28 16:26:16 -0500

    added devise admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  # devise_for :admins, controllers: {
+  #   sessions: 'admins/sessions'
+  # }
   get 'prayers/new'
   get 'home/index'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create!(email: "yhwhapparel@gmail.com", password: "helloworld", password_confirmation: "helloworld")
