ea2fd71cd4b70c6654017b82af818aba4ce44921
Mason Scott <mscott2757@berkeley.edu>
2017-05-30 22:28:49 -0500

    trying another thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,18 +80,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = {:host => 'yhwhapparel.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "yhwhapparel",
-    password: "fixedonjesus7"
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "yhwhapparel",
+    :password => "fixedonjesus7"
   }
 end
