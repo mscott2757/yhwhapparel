d4683ad7815aa510dcd9a1744114b181e0485fbb
Mason Scott <mscott2757@berkeley.edu>
2017-07-26 21:32:12 -0500

    changed font

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  #devise_for :admins
-  # devise_for :admins, controllers: {
-  #   sessions: 'admins/sessions'
-  # }
+  # devise_for :users
   get 'prayers/new'
   get 'home/index'
 
